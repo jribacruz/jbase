375dfd905aff802325feb46351a8792319832b53
jcruz <jcruz@jcruz>
2016-05-04 23:37:33 -0300

    Novos itens no mapa de acentos

--- jbase/src/main/resources/META-INF/resources/js/jbase/jbase.js
+++ jbase/src/main/resources/META-INF/resources/js/jbase/jbase.js
@@ -12,6 +12,48 @@ jbase = function() {
 			$(eId).effect("highlight", {}, 500);
 		}
 	}
+	
+	/**
+	 * 
+	 */
+	var search = function(qry) {
+
+	}
+
+	/**
+	 * Mapa de acentos.
+	 */
+	var accentMap = {
+		"Á" : "A",
+		"á" : "a",
+		"Ã" : "A",
+		"ã" : "a",
+		"É" : "E",
+		"é" : "e",
+		"Ê" : "E",
+		"ê" : "e",
+		"Í" : "I",
+		"í" : "i"
+		"Ó" : "O",
+		"ó" : "o",
+		"ö" : "o",
+		"Ô" : "O"
+		"ô" : "o",
+		"Ç" : "C"
+		"ç" : "c"
+	};
+
+	/**
+	 * Remove os acentos de utilizando a array accentMap para realizar a
+	 * substituição pelo caractere não acentuado.
+	 */
+	function _normalizeSearch(term) {
+		var ret = "";
+		for (var i = 0; i < term.length; i++) {
+			ret += accentMap[term.charAt(i)] || term.charAt(i);
+		}
+		return ret;
+	}
 
 	return {
 		highlight : highlight
