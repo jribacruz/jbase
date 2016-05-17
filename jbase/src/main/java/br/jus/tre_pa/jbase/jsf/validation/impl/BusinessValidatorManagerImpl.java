eb185bd61119601d4e077a69fd7cc89b8abb1912
jcruz <jcruz@jcruz>
2016-05-17 15:35:42 -0300

    Annotations

--- jbase/src/main/java/br/jus/tre_pa/jbase/jsf/validation/impl/BusinessValidatorManagerImpl.java
+++ jbase/src/main/java/br/jus/tre_pa/jbase/jsf/validation/impl/BusinessValidatorManagerImpl.java
@@ -37,6 +37,8 @@ public class BusinessValidatorManagerImpl<T> implements BusinessValidatorManager
 	private BusinessValidatorContext validatorContext;
 
 	/**
+	 * Chama todos os validadores para uma determinada entidade. Em caso de falha (exception BusinessValidationException lançada) as
+	 * mensagens de erro são acumuladas.
 	 * 
 	 */
 	@SuppressWarnings("unchecked")
@@ -46,24 +48,38 @@ public class BusinessValidatorManagerImpl<T> implements BusinessValidatorManager
 		if (bean != null) {
 			Class<T> beanClass = (Class<T>) bean.getClass();
 			Iterator<BusinessValidator<T>> iterValidatorsForBeanClass = validatorsMap.get(beanClass).iterator();
+			/*
+			 * Itera por todos o validadores (BusinessValidator) de uma determinada entidade T.
+			 */
 			while (iterValidatorsForBeanClass.hasNext()) {
 				BusinessValidator<T> validator = iterValidatorsForBeanClass.next();
+				/*
+				 * Verifica se o validator não está ignorado.
+				 */
 				if (!isIgnoredValidator(validator)) {
 					try {
 						validator.validate(bean);
 					} catch (BusinessValidationException e) {
 						validatorContext.addMessage(e.getMessage());
+						/*
+						 * Torna o estado de validação como falho.
+						 */
 						validatorContext.validationFailed();
 					}
 				}
 			}
-			if(validatorContext.isValidationFailed()) {
+			/*
+			 * Em caso de algum validador falhe uma exception BusinessValidationException é lançada impedindo a chamada do método de negócio
+			 * (insert, update etc...)
+			 */
+			if (validatorContext.isValidationFailed()) {
 				throw new BusinessValidationException();
 			}
 		}
 	}
 
 	/**
+	 * Checa a presença da annotation @Ignore na classe de Validação de Nogócio (BusinessValidator).
 	 * 
 	 * @param validator
 	 * @return
