cd0285b9865ac17d8c103272578518c2b7be7385
jcruz <jcruz@jcruz>
2016-05-18 15:58:21 -0300

    Novos itens do Workflow

---
+++ jbase/src/main/java/br/jus/tre_pa/jbase/jsf/workflow/interceptor/UpdateHeadersInterceptor.java
@@ -0,0 +1,59 @@
+package br.jus.tre_pa.jbase.jsf.workflow.interceptor;
+
+import java.io.Serializable;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+import br.gov.frameworkdemoiselle.util.Strings;
+import br.jus.tre_pa.jbase.jsf.validation.context.BusinessValidatorContext;
+import br.jus.tre_pa.jbase.jsf.workflow.annotation.UpdateHeader;
+import br.jus.tre_pa.jbase.jsf.workflow.annotation.UpdateHeaders;
+import br.jus.tre_pa.jbase.jsf.workflow.context.UIService;
+import br.jus.tre_pa.jbase.jsf.workflow.utils.InvocationContextUtil;
+
+@Interceptor
+@UpdateHeader
+public class UpdateHeadersInterceptor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private UIService service;
+
+	@Inject
+	private BusinessValidatorContext businessValidatorContext;
+
+	@AroundInvoke
+	public Object invoke(InvocationContext ic) throws Exception {
+		Object ret = ic.proceed();
+		if (!FacesContext.getCurrentInstance().isValidationFailed() && !businessValidatorContext.isValidationFailed()) {
+			UpdateHeaders headers = ic.getMethod().getAnnotation(UpdateHeaders.class);
+			for(UpdateHeader header: headers.value()) {
+				processUpdateHeader(ic, header);
+			}
+		}
+		return ret;
+	}
+
+	private String getForClass(InvocationContext ic, UpdateHeader updateHeader) {
+		Class<?> forClass = updateHeader.forClass();
+		if (forClass == Void.class) {
+			return InvocationContextUtil.getManagedBean(ic);
+		}
+		return forClass.getSimpleName();
+	}
+
+	private void processUpdateHeader(InvocationContext ic, UpdateHeader updateHeader) {
+		String forClassId = Strings.camelCaseToSymbolSeparated(getForClass(ic, updateHeader).replaceAll("MB", ""), "_");
+		String id = String.format("%s_form_id:%s_header_id", forClassId, forClassId);
+		service.update(id);
+	}
+
+}
---
+++ jbase/src/main/java/br/jus/tre_pa/jbase/jsf/workflow/interceptor/UpdateRegionsInterceptor.java
@@ -0,0 +1,61 @@
+package br.jus.tre_pa.jbase.jsf.workflow.interceptor;
+
+import java.io.Serializable;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+import br.gov.frameworkdemoiselle.util.Strings;
+import br.jus.tre_pa.jbase.jsf.workflow.annotation.UpdateRegion;
+import br.jus.tre_pa.jbase.jsf.workflow.annotation.UpdateRegions;
+import br.jus.tre_pa.jbase.jsf.workflow.context.UIService;
+import br.jus.tre_pa.jbase.jsf.workflow.utils.InvocationContextUtil;
+
+@Interceptor
+@UpdateRegions
+public class UpdateRegionsInterceptor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private UIService service;
+
+	@AroundInvoke
+	public Object invoke(InvocationContext ic) throws Exception {
+		Object ret = ic.proceed();
+		if (!FacesContext.getCurrentInstance().isValidationFailed()) {
+			UpdateRegions regions = ic.getMethod().getAnnotation(UpdateRegions.class);
+			for (UpdateRegion updateRegion : regions.value()) {
+				processUpdateRegion(ic, updateRegion);
+			}
+		}
+		return ret;
+	}
+
+	private String getForClass(InvocationContext ic, UpdateRegion updateRegion) {
+		Class<?> forClass = updateRegion.forClass();
+		if (forClass == Void.class) {
+			return InvocationContextUtil.getManagedBean(ic);
+		}
+		return forClass.getSimpleName();
+	}
+
+	private String[] getIds(UpdateRegion updateRegion) {
+		return updateRegion.id();
+	}
+
+	private void processUpdateRegion(InvocationContext ic, UpdateRegion updateRegion) {
+		String forClassId = Strings.camelCaseToSymbolSeparated(getForClass(ic, updateRegion).replaceAll("MB", ""), "_");
+		for (String id : getIds(updateRegion)) {
+			String componentId = String.format("%s_form_id:%s", forClassId, id);
+			service.update(componentId);
+		}
+	}
+
+}
